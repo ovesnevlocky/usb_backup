510105cdb7219f7c4b73a8e2bd77723ed7540c87
myfriend <myfriend.email>
2026-07-29 16:26:58 +0200

    stats added

--- try_dir/dirs.c
+++ try_dir/dirs.c
@@ -9,11 +9,31 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include <time.h>
+
+#include <sys/sysmacros.h>
+
+#include <stdint.h>
+
 bool isNull(void *a);
 
 
 void concat(char *dst, char *dir_to);
 
+void cleanDirTo(char *dst, char *path);
+
+	
+void cleanDirTo(char *cwd, char *dir_to)
+{
+
+	size_t lenDirTo = isNull(dir_to) ? 0 : strlen(dir_to);
+	
+	size_t offset = lenDirTo + 1;
+	size_t start = strlen(cwd) - offset;
+	//change back to the string before concating//
+	memset(cwd + start , 0,  offset);
+
+}
 bool isParentDir(char *d_name)
 {
 	return strcmp(d_name, "..") == 0;
@@ -24,13 +44,20 @@ bool isCurrDir(char *d_name)
 	return strcmp(d_name, ".") == 0;
 }	
 
-
+size_t numDir = 0;
+size_t numFile = 0;
 void openDir(char *cwd, char *dir_to)
 {
 
 	concat(cwd, dir_to);
 	//fprintf(stderr, "%s\n", cwd);
 	DIR *dirp = opendir(cwd);
+	if(dirp == NULL)
+	{
+		cleanDirTo(cwd, dir_to);
+		perror(cwd);
+		return;
+	}
         struct dirent *dp; 
 	
 	do	
@@ -51,25 +78,20 @@ void openDir(char *cwd, char *dir_to)
 				continue;	
 			}
 
-			fprintf(stdout, "-----open dir: %s -----\n", dp->d_name);
-			
+			//fprintf(stdout, "-----open dir: %s -----\n", dp->d_name);
+			numDir++;	
 			openDir (cwd, dp->d_name);
 		}
 		else if(dp ->d_type == DT_REG)
 		{
-			fprintf(stdout, "file: %s\n", dp->d_name);
+			numFile++;
+			//fprintf(stdout, "file: %s\n", dp->d_name);
 		}
 	}while(dirp);
 	//plus one for '/' char
-	fprintf(stdout, "-----close dir: %s -----\n\n", strcmp(dir_to, " ") == 0 ? "try_dir" : dir_to);
-
 	closedir(dirp);	
-	size_t lenDirTo = isNull(dir_to) ? 0 : strlen(dir_to);
-	
-	size_t offset = lenDirTo + 1;
-	size_t start = strlen(cwd) - offset;
-	//change back to the string before concating//
-	memset(cwd + start , 0,  offset);
+	cleanDirTo(cwd, dir_to);
+//	fprintf(stdout, "-----close dir: %s -----\n\n", strcmp(dir_to, " ") == 0 ? cwd : dir_to);
 	return;
 }
 
@@ -109,18 +131,79 @@ void concat(char *dst, char *dir_to)
 	return;
 }
 
-int main()
+
+void setHome(char *cwd)
 {
-	char cwd[PATH_MAX] = {0};
-	//getcwd(cwd, sizeof(cwd));
 	char *path = "/home/kazuy";
 	int i = 0;
 	for(; path[i] != '\0'; i++)
 		cwd[i] = path[i];
-
-
 	cwd[i] = '\0';
-	puts(cwd);
+}
+
+void getStat(char *path)
+{
+	struct stat sb;
+	
+    	if (lstat(path, &sb) == -1)	
+       	{
+        	perror("lstat");
+        	return;
+    	}
+
+	printf("ID of containing device:  [%x,%x]\n",
+               major(sb.st_dev),
+               minor(sb.st_dev));
+
+        printf("File type:                ");
+
+        switch (sb.st_mode & S_IFMT) 
+	{
+           case S_IFBLK:  printf("block device\n");            break;
+           case S_IFCHR:  printf("character device\n");        break;
+           case S_IFDIR:  printf("directory\n");               break;
+           case S_IFIFO:  printf("FIFO/pipe\n");               break;
+           case S_IFLNK:  printf("symlink\n");                 break;
+           case S_IFREG:  printf("regular file\n");            break;
+           case S_IFSOCK: printf("socket\n");                  break;
+           default:       printf("unknown?\n");                break;
+         }
+
+         printf("I-node number:            %ju\n", (uintmax_t) sb.st_ino);
+
+         printf("Mode:                     %jo (octal)\n",
+                (uintmax_t) sb.st_mode);
+
+         printf("Link count:               %ju\n", (uintmax_t) sb.st_nlink);
+         printf("Ownership:                UID=%ju   GID=%ju\n",
+                  (uintmax_t) sb.st_uid, (uintmax_t) sb.st_gid);
+
+         printf("Preferred I/O block size: %jd bytes\n",
+                 (intmax_t) sb.st_blksize);
+         printf("File size:                %jd bytes\n",
+                  (intmax_t) sb.st_size);
+         printf("Blocks allocated:         %jd\n",
+                 (intmax_t) sb.st_blocks);
+
+         printf("Last status change:       %s", ctime(&sb.st_ctime));
+         printf("Last file access:         %s", ctime(&sb.st_atime));
+         printf("Last file modification:   %s", ctime(&sb.st_mtime));
+
+
+
+}
+
+
+int main()
+{
+	char cwd[PATH_MAX] = {0};
+	getcwd(cwd, sizeof(cwd));
+	puts(cwd);	
+	getStat(cwd);
+
+//	setHome(cwd);	
+
 	openDir(cwd, " " );
+	printf("Dir: %lu, file: %lu\n", numDir, numFile);
 	
 }
