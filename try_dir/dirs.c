be9dbc6f36918fa666ddd96f698774947f5b31ed
myfriend <myfriend.email>
2026-07-30 11:37:55 +0200

    make copy was added

--- try_dir/dirs.c
+++ try_dir/dirs.c
@@ -20,30 +20,34 @@
 bool isNull(void *a);
 
 
-void concat(char *dst, char *dir_to);
+void concat(char *dst, const char *dir_to);
 
-void cleanDirTo(char *dst, char *path);
+void cleanDirTo(char *dst,const  char *path);
 
 bool getStat(char *path);
 
-char *cpyPath(char *path)
+char *cpyPath(const char *path)
 {
 	char *ret = malloc(sizeof(char) * (strlen(path) + 1));
 	memcpy(ret, path, strlen(path) + 1);
 
 	return ret;	
 }
-void cleanDirTo(char *cwd, char *dir_to)
+
+
+void cleanDirTo(char *cwd,const char *dir_to)
 {
 
 	size_t lenDirTo = isNull(dir_to) ? 0 : strlen(dir_to);
 	
+	//plus one for '/' char
 	size_t offset = lenDirTo + 1;
 	size_t start = strlen(cwd) - offset;
 	//change back to the string before concating//
 	memset(cwd + start , 0,  offset);
 
 }
+
 static inline bool isParentDir(char *d_name)
 {
 	return strcmp(d_name, "..") == 0;
@@ -62,6 +66,53 @@ static inline bool isHidden(char *d_name)
 
 size_t numDir = 0;
 size_t numFile = 0;
+
+
+bool copyFile(const char *cwd, const char *fname)
+{
+	FILE *fp = fopen(cwd, "rb");
+	if(!fp)
+	{
+		perror(cwd);
+		return false;
+	}
+	
+	char *pathUsb = "/mnt/usb/copied";
+
+	char saveTo[PATH_MAX] = {0};
+	memcpy(saveTo, pathUsb, strlen(pathUsb));
+	concat(saveTo, fname);
+
+	FILE *fp_out = fopen(saveTo, "wb");
+	if(!fp_out)
+	{
+		perror(cwd);
+		return false;
+	}
+
+	char buff[4096] = {0};
+
+	fprintf(fp_out, "original at: %s\n", cwd);
+	size_t byteWritten, byteRead;	
+	while(byteRead = fread(buff, 1, sizeof(buff), fp))
+	{
+		if(byteRead <= 0)
+			break;
+		byteWritten = fwrite(buff, 1, byteRead, fp_out);
+		if(byteWritten != byteRead)
+		{
+			printf("written :%lu, read: %lu\n",  byteWritten, byteRead);
+			perror("fwrite");
+			fclose(fp_out);
+			fclose(fp);
+			return false;
+		}	
+	}
+
+	fclose(fp_out);
+	fclose(fp);
+	return true;
+}
 
 
 void openDir(char *cwd, char *dir_to, char **list, int *count)
@@ -107,14 +158,15 @@ void openDir(char *cwd, char *dir_to, char **list, int *count)
 			{
 				//printf("%s was modified within a week\n", cwd);
 				list[*count] = cpyPath(cwd);
+				copyFile(cwd, dp->d_name);	
 				*count += 1;
+
 			}
 			cleanDirTo(cwd, dp->d_name);
 			numFile++;
 			//fprintf(stdout, "file: %s\n", dp->d_name);
 		}
 	}while(dirp);
-	//plus one for '/' char
 	closedir(dirp);	
 	cleanDirTo(cwd, dir_to);
 //	fprintf(stdout, "-----close dir: %s -----\n\n", strcmp(dir_to, " ") == 0 ? cwd : dir_to);
@@ -126,7 +178,7 @@ bool isNull(void *a)
 	return a == NULL;
 }
 
-void concat(char *dst, char *dir_to)
+void concat(char *dst,const  char *dir_to)
 {
 	if(isNull(dst))
 	{
@@ -160,7 +212,7 @@ void concat(char *dst, char *dir_to)
 
 void setHome(char *cwd)
 {
-	char *path = "/home/kazuy";
+	char *path = "/home/kazuy/ws";
 	int i = 0;
 	for(; path[i] != '\0'; i++)
 		cwd[i] = path[i];
@@ -244,13 +296,11 @@ int main()
 	puts(cwd);	
 
 	setHome(cwd);	
-	getStat(cwd);
+	//getStat(cwd);
 	char **list = malloc(sizeof(char *) * 100);	
 	int count = 0;	
 	openDir(cwd, " ", list, &count);
 	printf("Dir: %lu, file: %lu\n", numDir, numFile);
-
-	printf("count is %i\n", count);
 
 	for(int i = 0; i < count; i++)
 		free(list[i]);
